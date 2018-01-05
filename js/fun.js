384ffbf688dde1c9d509fbca0758d0d9ba15551d
Daniel S <budartz@gmail.com>
2018-01-05 21:59:15 +0000

    spliting bug
    spliting bug - almost fixed. not sure what I did (to be honest) but think it was the order that I had set the variables

--- js/fun.js
+++ js/fun.js
@@ -1,34 +1,32 @@
 $(document).ready(function(){
 
 	$('button').on('click', function(e) {
-	  	
-	  	e.preventDefault();
-        
-		let allnames = $('textarea').val().split('\n');
-		let namespergroup = Math.ceil(allnames.length / 2);
-		let allnameslen = allnames.length;
+		e.preventDefault();
+
+		let allnames = $('textarea').val().split('\n'),
+			namespergroup = Math.ceil(allnames.length / 2),
+			allnameslen = allnames.length;
 
 		let numgroups = Math.ceil(allnameslen / namespergroup);
+		
 
 
-		for (let i = 0; i <numgroups; i+=1) {
-			$('.groups').append('<div class="group" id="group' + (i+1) + '"><h1>Team ' + (i+1) + '</h1></div>');
+		for (let i = 0; i < numgroups; i+=1) {
+			$('.groups').append('<div class="group" id="group' + (i+1) + '"><h2>Group ' + (i+1) + '</h2></div>');
 		}
 
-	    $('.group').each(function() {
-				for (let j=0;j<=namespergroup; j+=1){
+		$('.group').each(function() {
+			for (let j = 0; j < namespergroup; j+=1) {
 
-					let rand = Math.floor(Math.random() * allnames.length);
+				let rand = Math.floor(Math.random() * allnames.length);
 
-				 	if(allnames[rand]){
-				 		$(this).append('<p>' + allnames[rand] + '</p>');
-					}
-					allnames.splice(rand, 1);
-	 				console.log(allnames);	
-				}			
-		});		
+				if(allnames[rand]){
+					$(this).append('<p>' + allnames[rand] + '</p>');
+				}
+				allnames.splice(rand, 1);
+				console.log(allnames);
+			}
+		});
 	});
+
 });
-
-
-
